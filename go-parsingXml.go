53d15e16e7e67e9b5d1225d6fe3a9a1513304cf2
niranjann-evolvus <niranjan.n@evolvussolutions.com>
2021-03-04 22:35:36 +0530

    http and xml parsing

---
+++ go-parsingXml.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type SiteMapIndex struct {
+	Locations []location `xml:"sitemap"`
+}
+
+type location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+func main() {
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	var s SiteMapIndex
+	xml.Unmarshal(bytes, &s)
+	fmt.Println(s.Locations)
+
+}
