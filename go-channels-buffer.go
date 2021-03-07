31061637caf7f4f714cc7cd80d15a5fc3650bdc3
niranjann-evolvus <niranjan.n@evolvussolutions.com>
2021-03-07 21:20:46 +0530

    channels and buuffer

---
+++ go-channels-buffer.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func foo(i int, c chan int) {
+	defer wg.Done()
+	c <- i * 2
+
+}
+func main() {
+
+	ch := make(chan int, 10)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go foo(i, ch)
+	}
+	wg.Wait()
+	close(ch)
+	for value := range ch {
+		fmt.Println(value)
+	}
+}
